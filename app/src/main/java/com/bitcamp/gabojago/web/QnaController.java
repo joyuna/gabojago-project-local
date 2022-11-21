8de545b351ac741f575066a51e1c8dd3e6e64a6e
joyounga <joayounga@gmail.com>
2022-11-21 11:44:32 +0900

    Feat: QnA 게시글 비공개 여부에 따른 접근 권한 기능
    게시글 작성자와 관리자외 접근 제한

--- app/src/main/java/com/bitcamp/gabojago/web/QnaController.java
+++ app/src/main/java/com/bitcamp/gabojago/web/QnaController.java
@@ -1,6 +1,7 @@
 package com.bitcamp.gabojago.web;
 
 import com.bitcamp.gabojago.service.QnaService;
+import com.bitcamp.gabojago.vo.Member;
 import com.bitcamp.gabojago.vo.qna.QnaBoard;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
@@ -37,26 +39,74 @@ public class QnaController {
     public String add(
             QnaBoard qnaBoard,
             HttpSession session) throws Exception {
+        Member member = (Member) session.getAttribute("loginMember");
         System.out.println("qnaControllerAdd : " + qnaBoard.toString());
-        qnaBoard.setId("user1"); // 로그인전 테스트 위해 임의로 넣은 값
+        qnaBoard.setId(member.getId());
         qnaService.add(qnaBoard);
         return "redirect:list";
     }
 
+//    @GetMapping("detail")
+//    public ModelAndView detail(int no, HttpSession session) throws Exception {
+//        Member member = (Member) session.getAttribute("loginMember");
+//        ModelAndView mv = new ModelAndView();
+//        QnaBoard qnaBoard = qnaService.get(no);
+//        if (member == null) {
+//            mv.setViewName("/support/qna/list");
+//            return mv;
+//        } else if (!member.getId().equals(qnaBoard.getId()) && !member.getId().equals("admin")) {
+//            mv.setViewName("/support/qna/list");
+//            return mv;
+//        }
+//        mv.setViewName("/support/qna/detail?no=" + no);
+//        mv.addObject("qnaBoard", qnaBoard);
+//        return mv;
+//    }
+
+//    @GetMapping("detail")
+//    public String detail(int no, Model model, HttpSession session) throws Exception {
+//        Member member = (Member) session.getAttribute("loginMember");
+//        QnaBoard qnaBoard = qnaService.get(no);
+//        if (member == null) {
+//            return "redirect:list";
+//        } else if (!member.getId().equals(qnaBoard.getId()) && !member.getId().equals("admin")) {
+//            return "redirect:list";
+//        }
+//        model.addAttribute("qnaBoard", qnaBoard);
+//        return "redirect:detail?no="+no;
+//    }
+
     @GetMapping("detail")
-    public void detail(int no, Model model) throws Exception {
-        model.addAttribute("qnaBoard", qnaService.get(no));
+    public void detail(int no, Model model, HttpSession session) throws Exception {
+        Member member = (Member) session.getAttribute("loginMember");
+        QnaBoard qnaBoard = qnaService.get(no);
+        model.addAttribute("qnaBoard", qnaBoard);
+        boolean isPass = true;
+
+        if(qnaBoard.getDisclosure() && member == null) {
+            isPass = false;
+        } else if(qnaBoard.getDisclosure() && member != null) {
+            if(!member.getId().equals(qnaBoard.getId()) && !member.getId().equals("admin")) {
+                isPass = false;
+            }
+        }
+
+        model.addAttribute("isPass", isPass);
     }
 
     @GetMapping("delete")
-    public String delete(int no) throws Exception {
+    public String delete(int no, HttpSession session) throws Exception {
+        Member member = (Member) session.getAttribute("loginMember");
+
         qnaService.delete(no);
         return "redirect:list";
     }
 
     @GetMapping("editForm")
-    public void editForm(int no, Model model) throws Exception {
+    public void editForm(int no, Model model, HttpSession session) throws Exception {
+        Member member = (Member) session.getAttribute("loginMember");
         model.addAttribute("qnaBoard", qnaService.get(no));
+        model.addAttribute("member", member);
     }
 
     @PostMapping("update")
@@ -64,7 +114,12 @@ public class QnaController {
             QnaBoard qnaBoard,
             HttpSession session) throws Exception {
         System.out.println("qnaControllerUpdate : " + qnaBoard.toString());
-        qnaBoard.setId("user1"); // 로그인전 테스트 위해 임의로 넣은 값
+        Member member = (Member) session.getAttribute("loginMember");
+        if (member == null) {
+            return "redirect:list";
+        } else if (!member.getId().equals(qnaBoard.getId())) {
+            return "redirect:list";
+        }
         qnaService.update(qnaBoard);
         return "redirect:list";
     }
